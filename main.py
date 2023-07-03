e8fe56f04860b9baaddbe1d82e7ed8aad8f24550
zakhalex <50172897+zakhalex@users.noreply.github.com>
2023-07-03 08:28:32 -0400

    ZAKHALEK-0000 - Create main.py
    The initial code is from DLAI course

---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+import openai
+import tiktoken
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.environ['OPENAI_API_KEY']
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0,
+    )
+    return response.choices[0].message["content"]
+
+def get_completion_from_messages(messages, 
+                                 model="gpt-3.5-turbo", 
+                                 temperature=0, 
+                                 max_tokens=500):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+        max_tokens=max_tokens, # the maximum number of tokens the model can ouptut 
+    )
+    return response.choices[0].message["content"]
+
+
+def get_completion_and_token_count(messages, 
+                                   model="gpt-3.5-turbo", 
+                                   temperature=0, 
+                                   max_tokens=500):
+    
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, 
+        max_tokens=max_tokens,
+    )
+    
+    content = response.choices[0].message["content"]
+    
+    token_dict = {
+'prompt_tokens':response['usage']['prompt_tokens'],
+'completion_tokens':response['usage']['completion_tokens'],
+'total_tokens':response['usage']['total_tokens'],
+    }
+
+    return content, token_dict
+
+messages = [
+{'role':'system', 
+ 'content':"""You are an assistant who responds\
+ in the style of Dr Seuss."""},    
+{'role':'user',
+ 'content':"""write me a very short poem \ 
+ about a happy carrot"""},  
+] 
+response, token_dict = get_completion_and_token_count(messages)
+
+print(response)
+print(token_dict)
