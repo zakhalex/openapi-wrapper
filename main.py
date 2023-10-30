998891f6fb42ab1746da85720f1a863452ee0949
zakhalex <50172897+zakhalex@users.noreply.github.com>
2023-10-30 10:06:17 -0400

    ZAKHALEK-0000 Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ messages = [
  in the style of Dr Seuss."""},    
 {'role':'user',
  'content':"""write me a very short poem \ 
- about a happy carrot"""},  
+ about a curious bot"""},  
 ] 
 response, token_dict = get_completion_and_token_count(messages)
 
